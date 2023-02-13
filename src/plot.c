51e890b9cc792488556a21d0e374e39a67e332b2
Asier Septién <yoshimanbiz@gmail.com>
2023-02-13 12:29:14 +0100

    Combined su+eff plot

--- src/plot.c
+++ src/plot.c
@@ -22,6 +22,24 @@ void plot_graphics(bm_t *bm){
 		"plot 'out/results.dat' using 2:xticlabels(1) t '' with linespoints ls 1"
 	};
 
+	// Speedup - Efficiency
+	char * cmdseq2[] = {
+		"set term png", // export as .png
+		"set title 'Speedup - Efficiency'", 
+		"set xlabel 'Processors'",
+		"set ylabel 'Speedup factor'",
+		"set y2label 'Efficiency factor'",
+		"set format y2 '%g%%'",
+		"set y2tics",
+		//"set style data lines", // line graph
+		"set style line 1 lc rgb '#e8096a' lt 1 lw 3 pt 6 ps 1", // linecolor magenta, linewidth 2.5, round points, pointsize 2
+		"set style line 2 lc rgb '#d1b70a' lt 1 lw 3 pt 6 ps 1", // linecolor gold, linewidth 2.5, round points, pointsize 2
+		"set output 'out/plot_su-eff.png'",
+		"plot 'out/results.dat' using 3:xticlabels(1) t 'Speedup' with linespoints ls 1 axis x1y1,\
+		'out/results.dat' using ($4*100) t 'Efficiency' with linespoints ls 2 axis x1y2"
+	};
+
+	/*
 	// Speedup
 	char * cmdseq2[] = {
 		"set term png", // export as .png
@@ -45,7 +63,7 @@ void plot_graphics(bm_t *bm){
 		"set output 'out/plot_eff.png'",
 		"plot 'out/results.dat' using 4:xticlabels(1) t '' with linespoints ls 1"
 	};
-
+	*/
 
 	// Send commands to gnuplot one by one.
 	int i;
@@ -55,8 +73,10 @@ void plot_graphics(bm_t *bm){
 	for (i = 0; i < LENGTH(cmdseq2); i++)
 		fprintf(gnuplotPipe, "%s \n", cmdseq2[i]); 
 	
+	/*
 	for (i = 0; i < LENGTH(cmdseq3); i++)
 		fprintf(gnuplotPipe, "%s \n", cmdseq3[i]); 
+	*/
 
 	pclose(gnuplotPipe);
 }
